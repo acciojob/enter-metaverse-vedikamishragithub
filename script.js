8ba5c4e0758f5984449691542abcfc71bf8a6f93
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-19 14:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
 enterBtn.addEventListener("click",()=>{
 	var para=document.getElementById("status");
+
 	para.innerText="Entered Metaverse";
+	para.style.cssText="color:Dark Black; font-size:50px;font-weight: bold;";
+	
 	
 })
 
